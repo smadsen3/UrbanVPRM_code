f1d0e27f2091d705d48818f193f423e16a487029
Sabrina <sabrina.madsen@mail.utoronto.ca>
2022-02-16 12:06:27 -0500

    Editted directories to run on my computer

--- processLC_ISA_script1.R
+++ processLC_ISA_script1.R
@@ -16,7 +16,7 @@ library("rgdal")
 library("shapefiles")
 
 ## Create Directories
-setwd("/projectnb/buultra/iasmith/VPRM_urban_30m")
+setwd("C:/Users/kitty/Documents/Research/SIF/UrbanVPRM/UrbanVPRM/dataverse_files/")
 city = 'NIST30'
 print("Create directories for NLCD data")
 dir.create(paste0(city),showWarnings = FALSE)
@@ -30,18 +30,18 @@ NLCD_CRS = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0
 
 ## Import a LANDSAT file cropped to study domain to define grid and extent of the study area.
 # Project the grid into NLCD projection to subset NLCD data.
-LANDSAT_area =  raster('/projectnb/buultra/iasmith/VPRM_urban_30m/NIST30/landsat/landsat8/ls0113_8.tif') # landsat data in /urbanVPRM_30m/driver_data/landsat/
+LANDSAT_area =  raster('C:/Users/kitty/Documents/Research/SIF/UrbanVPRM/UrbanVPRM/dataverse_files/NIST30/landsat/landsat8/ls0113_8.tif') # landsat data in /urbanVPRM_30m/driver_data/landsat/
 npixel = ncell(LANDSAT_area)
 print(paste0("number of pixels = ",npixel))
 grid_NLCD = projectRaster(LANDSAT_area,crs=NLCD_CRS)  
 
 ## Import bounding box defining study domain
-bound_box = readOGR(dsn="/projectnb/buultra/iasmith/VPRM_urban_30m/NIST30/shapefiles",layer='NIST_30m_BB') # shapefile data in /urbanVPRM_30m/shapefiles/
+bound_box = readOGR(dsn="C:/Users/kitty/Documents/Research/SIF/UrbanVPRM/UrbanVPRM/dataverse_files/NIST30/shapefiles",layer='NIST_30m_BB') # shapefile data in /urbanVPRM_30m/shapefiles/
 bound_box = spTransform(bound_box, LANDSAT_CRS)
 
 ## Import, reproject, and crop LC and ISA data
 # Land Cover
-LC_USA = raster("/projectnb/buultra/iasmith/VPRM_urban_30m/NLCD/LC/nlcd_2019_land_cover_l48_20210604.img") # Land Cover data from https://www.mrlc.gov/data
+LC_USA = raster("C:/Users/kitty/Documents/Research/SIF/UrbanVPRM/UrbanVPRM/dataverse_files/NLCD/LC/nlcd_2019_land_cover_l48_20210604.img") # Land Cover data from https://www.mrlc.gov/data
 LC_USAcrop = crop(LC_USA,grid_NLCD)
 extent(LC_USAcrop) = extent(grid_NLCD)
 LClandsat = projectRaster(LC_USAcrop,crs=LANDSAT_CRS)
@@ -49,7 +49,7 @@ LClandsatcrop = crop(LClandsat,LANDSAT_area)
 LClandsatcrop = mask(LClandsatcrop,bound_box)
 
 # ISA
-ISA_USA = raster("/projectnb/buultra/iasmith/VPRM_urban_30m/NLCD/ISA/nlcd_2019_impervious_l48_20210604.img") # Impervious data from https://www.mrlc.gov/data
+ISA_USA = raster("C:/Users/kitty/Documents/Research/SIF/UrbanVPRM/UrbanVPRM/dataverse_files/NLCD/ISA/nlcd_2019_impervious_l48_20210604.img") # Impervious data from https://www.mrlc.gov/data
 
 ISA_USAcrop = crop(ISA_USA,grid_NLCD)
 extent(LC_USAcrop) = extent(grid_NLCD)
@@ -64,7 +64,7 @@ lc_resample <- resample(LClandsatcrop, LANDSAT_area, method = 'ngb')
 isa_resample <- resample(ISAlandsatcrop, LANDSAT_area, method = 'ngb')
 
 ## Write Rasters
-writeRaster(lc_resample,filename="/projectnb/buultra/iasmith/VPRM_urban_30m/Landcover/LC_NIST.tif",
+writeRaster(lc_resample,filename="C:/Users/kitty/Documents/Research/SIF/UrbanVPRM/UrbanVPRM/dataverse_files/NIST30/Landcover/LC_NIST.tif",
             overwrite=TRUE)
-writeRaster(isa_resample,filename="/projectnb/buultra/iasmith/VPRM_urban_30m/ISA/ISA_NIST.tif",
+writeRaster(isa_resample,filename="C:/Users/kitty/Documents/Research/SIF/UrbanVPRM/UrbanVPRM/dataverse_files/NIST30/ISA/ISA_NIST.tif",
             overwrite=TRUE)
